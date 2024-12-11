b815d3d544abdea60d6bb4e578a99420ec0bb57b
kien <dp1.1a2kien@gmail.com>
2024-12-11 23:02:32 +0700

    edit

--- api/v1/controller/task.controller.ts
+++ api/v1/controller/task.controller.ts
@@ -121,3 +121,19 @@ export const create = async (req: Request, res: Response) => {
     });
   }
 };
+//[patch] api/v1/task/edit
+export const edit = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    await Task.updateOne({ _id: id }, req.body);
+    res.json({
+      code: 200,
+      message: "Chỉnh sửa thành công",
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Chỉnh sửa thất bại",
+    });
+  }
+};
