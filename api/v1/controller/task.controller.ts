d2ffe22b3377b5af6275f5ea9d9512fa8f1a73ca
kien <dp1.1a2kien@gmail.com>
2024-12-11 16:23:44 +0700

    sort

--- api/v1/controller/task.controller.ts
+++ api/v1/controller/task.controller.ts
@@ -2,13 +2,22 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 //[get] api/v1/task/
 export const index = async (req: Request, res: Response) => {
+  //find
   const find = {
     deleted: false,
   };
   if (req.query.status) {
     find["status"] = req.query.status;
   }
-  const task = await Task.find(find);
+  //end find
+  //sort
+  const sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    const sortKey = req.query.sortKey.toLocaleString();
+    sort[sortKey] = req.query.sortValue;
+  }
+  //end sort
+  const task = await Task.find(find).sort(sort);
 
   res.json(task);
 };
