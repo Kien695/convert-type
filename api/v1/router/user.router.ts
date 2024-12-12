423a71a5f7f8b7a68c44adcd56a30a4f27ea4b52
kien <dp1.1a2kien@gmail.com>
2024-12-12 10:06:52 +0700

    login

--- api/v1/router/user.router.ts
+++ api/v1/router/user.router.ts
@@ -3,5 +3,6 @@ const router: Router = Router();
 
 import * as controller from "../controller/user.controller";
 router.post("/register", controller.register);
+router.post("/login", controller.login);
 
 export const userRouter: Router = router;
