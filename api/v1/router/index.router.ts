63d4b55fc02e5ff67213da8c0e443ba7c002885f
kien <dp1.1a2kien@gmail.com>
2024-12-12 10:51:27 +0700

    chuyển thành prive

--- api/v1/router/index.router.ts
+++ api/v1/router/index.router.ts
@@ -1,9 +1,10 @@
 import { taskRouter } from "./task.router";
 import { userRouter } from "./user.router";
 import { Express } from "express";
+import * as authMiddleware from "../middleware/auth.middleware";
 const mainV1Router = (app: Express): void => {
   const version = "/api/v1";
-  app.use(version + "/task", taskRouter);
+  app.use(version + "/task", authMiddleware.requireAuth, taskRouter);
   app.use(version + "/user", userRouter);
 };
 export default mainV1Router;
